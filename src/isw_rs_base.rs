b1af3ea58ae604d9b1d23694cca8d47d8dc088a7
Tobias Felix Egger <egtobi@gmail.com>
2021-07-10 01:52:12 +0200

    Added USB_BACKLIGHT

--- src/isw_rs_base.rs
+++ src/isw_rs_base.rs
@@ -1,9 +1,15 @@
 use crate::isw_raw_access::IswRawAccess;
 use crate::isw_config_ops::IswConfigOps;
 
+pub enum UsbBacklightKind {
+    Off,
+    Half,
+    Full
+}
+
 pub struct IswRsBase {
     pub raw_access: IswRawAccess,
-    m_config_ops : IswConfigOps
+    m_config_ops: IswConfigOps,
 }
 
 impl IswRsBase {
@@ -12,13 +18,18 @@ impl IswRsBase {
     const COOLER_BOOST_ON: &'static str = "cooler_boost_on";
     const COOLER_BOOST_ADDRESS_IDENTIFIER: &'static str = "cooler_boost_address";
     const BATTERY_CHARGING_THRESHOLD_ADDRESS_IDENTIFIER: &'static str = "battery_charging_threshold_address";
+    const USB_BACKLIGHT: &'static str = "USB_BACKLIGHT";
+    const USB_BACKLIGHT_ADDRESS_IDENTIFIER: &'static str = "usb_backlight_address";
+    const USB_BACKLIGHT_OFF: &'static str = "usb_backlight_off";
+    const USB_BACKLIGHT_HALF: &'static str = "usb_backlight_half";
+    const USB_BACKLIGHT_FULL: &'static str = "usb_backlight_full";
     const IO_FILE: &'static str = "/sys/kernel/debug/ec/ec0/io";
     //const IO_FILE: &'static str = "dump.sys";
 
     pub fn new(cfg_file: String) -> IswRsBase {
         let mut s = IswRsBase {
             raw_access: IswRawAccess::new(IswRsBase::IO_FILE.to_string()),
-            m_config_ops: IswConfigOps::new(cfg_file)
+            m_config_ops: IswConfigOps::new(cfg_file),
         };
 
         s.m_config_ops.load_config();
@@ -26,7 +37,26 @@ impl IswRsBase {
         return s;
     }
 
-    pub fn set_battery_threshold(&mut self, t : u8) -> bool{
+    pub fn set_usb_backlight(&mut self, state: UsbBacklightKind) {
+        let value: u64;
+        let base_address = self.m_config_ops.get_base_address(IswRsBase::USB_BACKLIGHT.to_string(), IswRsBase::USB_BACKLIGHT_ADDRESS_IDENTIFIER.to_string());
+
+        match state {
+            UsbBacklightKind::Off => {
+                value = self.m_config_ops.get_numeric_property(IswRsBase::USB_BACKLIGHT.to_string(), IswRsBase::USB_BACKLIGHT_OFF.to_string());
+            }
+            UsbBacklightKind::Half => {
+                value = self.m_config_ops.get_numeric_property(IswRsBase::USB_BACKLIGHT.to_string(), IswRsBase::USB_BACKLIGHT_HALF.to_string());
+            }
+            UsbBacklightKind::Full => {
+                value = self.m_config_ops.get_numeric_property(IswRsBase::USB_BACKLIGHT.to_string(), IswRsBase::USB_BACKLIGHT_FULL.to_string());
+            }
+        }
+
+        self.raw_access.write_hw(base_address, value as u16);
+    }
+
+    pub fn set_battery_threshold(&mut self, t: u8) -> bool {
         if t >= 20 && t <= 100 {
             let base_address = self.m_config_ops.get_base_address(IswRsBase::COOLER_BOOST.to_string(), IswRsBase::BATTERY_CHARGING_THRESHOLD_ADDRESS_IDENTIFIER.to_string());
             self.raw_access.write_hw(base_address, (t as u16) + 128);
